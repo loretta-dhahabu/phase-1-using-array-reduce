dd7ec4f3517c7111e4105d0da91a9925bc5db88b
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-09 19:25:52 +0300

    commit tree 32dc14c2b20d5f6c091f0389cd1c5742cfc81407

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
+const reducer = (batteries, batch) => {
+  return (batteries += batch);
+};
+
+let totalBatteries = batteryBatches.reduce(reducer, 0);
+console.log( totalBatteries );
+
+
+
+
 // Code your solution here
+// const totalBatteries = batteryBatches.reduce((accum, batch) => {
+//   return (accum += batch);
+// }, 0);
+
+// const totalBatteries = batteryBatches.reduce(function (accumulator, element) {
+//   return element + accumulator;
+// }, 0);
+
+//function batteries() {
+  //return totalBatteries(batteryBatches.sum(31));
+  // let totalBatteries = 0;
+//}
+
+
+
